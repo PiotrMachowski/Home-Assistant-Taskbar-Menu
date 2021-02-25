89bbaf180b0a61e1128bcc2a06f8aec2e8a35a47
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2021-02-25 01:06:10 +0100

    Bump version to 1.1.2.0

--- Home Assistant Taskbar Menu/Properties/AssemblyInfo.cs
+++ Home Assistant Taskbar Menu/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Piotr Machowski")]
 [assembly: AssemblyProduct("Home Assistant Taskbar Menu")]
-[assembly: AssemblyCopyright("Copyright © 2020 Piotr Machowski")]
+[assembly: AssemblyCopyright("Copyright © 2021 Piotr Machowski")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,6 +50,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
