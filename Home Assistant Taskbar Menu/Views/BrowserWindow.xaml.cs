26b87c41e874789ae80447b7778950e0e70c7188
Mateusz Drab <madrab@microsoft.com>
2022-06-13 21:36:45 +0100

    Timer based focus loss detection

--- Home Assistant Taskbar Menu/Views/BrowserWindow.xaml.cs
+++ Home Assistant Taskbar Menu/Views/BrowserWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 using CefSharp;
 using CefSharp.Wpf;
 using Home_Assistant_Taskbar_Menu.Utils;
@@ -13,6 +16,18 @@ namespace Home_Assistant_Taskbar_Menu.Views
     /// </summary>
     public partial class BrowserWindow : Window
     {
+
+        private IntPtr _windowHandle;
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            _windowHandle = new WindowInteropHelper(this).Handle;
+        }
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        private System.Timers.Timer focusLossTimer = new System.Timers.Timer();
+
+
         private string Url { get; }
 
         public BrowserWindow(Configuration configuration)
@@ -35,6 +50,18 @@ namespace Home_Assistant_Taskbar_Menu.Views
                 Width = position.Value.width;
                 Height = position.Value.height;
             }
+
+            
+            focusLossTimer.Elapsed += (s, e) => {
+            if(GetForegroundWindow() != _windowHandle && Visibility == Visibility.Visible)
+                {
+                    Debug.WriteLine("Hiding browser window due to focus loss");
+                    Application.Current.Dispatcher.Invoke(new Action(() => { Hide(); }));
+                }
+            }; 
+            focusLossTimer.Interval = 500;
+            focusLossTimer.Enabled = true;           
+            
         }
 
         private void MinimizeButton(object sender, RoutedEventArgs e)
@@ -50,6 +77,7 @@ namespace Home_Assistant_Taskbar_Menu.Views
         private void CloseButton(object sender, RoutedEventArgs e)
         {
             Hide();
+            focusLossTimer.Stop();
         }
 
         private void HeaderMouseDown(object sender, MouseButtonEventArgs e)
@@ -71,6 +99,11 @@ namespace Home_Assistant_Taskbar_Menu.Views
         private void BrowserWindow_OnClosed(object sender, EventArgs e)
         {
             Storage.SavePosition((Left, Top, Width, Height));
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            focusLossTimer.Start();
         }
     }
 }
